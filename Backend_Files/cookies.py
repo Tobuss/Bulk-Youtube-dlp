08bc939326e0d4cd95d28604c129dadea49fbe08
Toby Waterworth <33454600+Tobuss@users.noreply.github.com>
2024-09-17 13:33:09 -0400

    Update cookies.py
    Due to chrome update, previous method was broken, using firefox now

--- Backend_Files/cookies.py
+++ Backend_Files/cookies.py
@@ -2,13 +2,13 @@ import browser_cookie3
 import os
 
 def secure_to_str(value):
-    return "TRUE" if value == 1 else "FALSE"
+    return "TRUE" if value else "FALSE"
 
 def export_cookies_to_netscape(output_file, target_site):
     try:
-        cookies = browser_cookie3.chrome()
+        cookies = browser_cookie3.firefox()
     except Exception as e:
-        print(f"Failed to load cookies from Google Chrome: {e}")
+        print(f"Failed to load cookies from Firefox: {e}")
         return
 
     with open(output_file, 'w') as file:
@@ -17,19 +17,20 @@ def export_cookies_to_netscape(output_file, target_site):
 
         for cookie in cookies:
             if target_site in cookie.domain:
-                include_subdomains = secure_to_str
-
+                include_subdomains = "TRUE"  # Assuming include subdomains for Netscape format
                 secure = secure_to_str(cookie.secure)
+                expiry = cookie.expires if cookie.expires else 0
 
-                expiry = cookie.expires if cookie.expires is not None else 0
-
-                file.write(f"{cookie.domain}\t{'TRUE'}\t{cookie.path}\t"
+                file.write(f"{cookie.domain}\t{include_subdomains}\t{cookie.path}\t"
                            f"{secure}\t{expiry}\t{cookie.name}\t{cookie.value}\n")
 
-    print(f"Cookies exported from {target_site} in Google Chrome to {output_file} successfully!")
+    print(f"Cookies exported from {target_site} in Firefox to {output_file} successfully!")
 
 if __name__ == "__main__":
-    output_file = ".\cookies.txt"
+    # Prepare the path for the output file in the parent folder
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    output_file = os.path.join(script_dir, '..', 'cookies.txt')
+
     target_site = ".youtube.com"
 
     export_cookies_to_netscape(output_file, target_site)
